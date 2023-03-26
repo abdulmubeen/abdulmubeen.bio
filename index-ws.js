9e2624668ceed83edc640ba8332b00586b985de0
Abdul Mubeen <abdulmubeen57@gmail.com>
2023-03-26 15:15:40 +0530

    Created Database using Sqlite

--- index-ws.js
+++ index-ws.js
@@ -15,6 +15,15 @@ const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({ server: server });
 
+process.on("SIGINT", () => {
+  server.close(() => {
+    wss.clients.forEach(function each(client) {
+      client.close();
+    });
+    shutdownDB();
+  });
+});
+
 wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
   console.log("Clients Connected", numClients);
@@ -24,6 +33,9 @@ wss.on("connection", function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send("Welcome to my server");
   }
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES(${numClients}, datetime('now'))
+  `);
 
   ws.on("close", function close() {
     wss.broadcast(`Current Visitors: ${numClients}`);
@@ -36,3 +48,27 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  console.log("Shutting Down");
+  getCounts();
+  db.close();
+}
